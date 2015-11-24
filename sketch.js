0cf9946de1ccea3031e434e787e3bffd4d4bdd8d
faaip <frederikjuutilainen@gmail.com>
2015-11-24 15:35:25 +0100

    GUI added

--- sketch.js
+++ sketch.js
@@ -1,35 +1,50 @@
 var video;
 var videoWidth, videoHeight;
+var sizeSlider;
+var backgroundSlider;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   video = createCapture(VIDEO);
-  video.size(640, 480);
+  video.size(1280, 1024);
   videoWidth = video.width;
   videoHeight = video.height;
-  document.title = random(1000, 9999) + "" + random(1000, 9999) + "" + random(1000, 9999) + "" + random(1000, 9999) + "";
+  document.title = "PRESS SHIFT TO ACCESS CONTROLS"
+
+  // GUI
+  // create sliders
+  sizeSlider = createSlider(1, 25, 12);
+  sizeSlider.position(0, 0);
+  sizeSlider.hide();
+  backgroundSlider = createSlider(0, 255, 255);
+  backgroundSlider.position(0, 20);
+  backgroundSlider.hide();
+  thresholdSlider = createSlider(0, 255 * 3, 0);
+  thresholdSlider.position(0, 40);
+  thresholdSlider.hide();
+  depthSensitivtySlider = createSlider(0, 1000, 300);
+  depthSensitivtySlider.position(0, 60);
+  depthSensitivtySlider.hide();
+  pixelationSlider = createSlider(3, 50, 12);
+  pixelationSlider.position(0, 80);
+  pixelationSlider.hide();
+
 }
 
 function draw() {
   rotateY(PI); // Flipping image
-  print(mouseX);
-  // Key controls
-  if (keyIsDown(CONTROL)) {
-    rotateX(frameCount * 0.005);
+  if (!keyIsDown(SHIFT)) {
+    // Orbitcontrol only works when GUI is hidden
+    orbitControl();
+    showGUI(false);
+  } else {
+    showGUI(true);
   }
-
-  if (keyIsDown(OPTION)) {
-    rotateX(frameCount * 0.005);
-
-    rotateZ(frameCount * 0.005);
-  }
-  orbitControl();
-  background(255);
-  ambientLight(random(180, 255));
-
-
+  background(backgroundSlider.value());
+  ambientLight(255);
   translate(-video.width / 2, -video.height / 2, 0);
-  var size = 12;
+  var size = pixelationSlider.value();
   var videopixels = video.loadPixels().pixels;
   for (var y = 0; y < video.height; y += size) {
     for (var x = 0; x < video.width; x += size) {
@@ -37,11 +52,12 @@ function draw() {
       var r = videopixels[i]
       var g = videopixels[i + 1]
       var b = videopixels[i + 2]
-      specularMaterial(r, g, b);
-      var z = -(r+g+b)/2;
+
+      specularMaterial(r, g, b); // Color boxes
+      var z = -(r + g + b) * (depthSensitivtySlider.value() / 1000);
       translate(size, 0, z);
-      if (r + g + b > mouseX - width / 2) {
-        box(map(mouseY, 0, height, 1, 15));
+      if (r + g + b > thresholdSlider.value()) {
+        box(sizeSlider.value());
       }
       translate(0, 0, -z); // New "line" of blocks
     }
@@ -52,4 +68,21 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function showGUI(b) {
+  // Shows and hides sliders
+  if (b) {
+    sizeSlider.show();
+    backgroundSlider.show();
+    thresholdSlider.show();
+    depthSensitivtySlider.show();
+    pixelationSlider.show();
+  } else {
+    sizeSlider.hide();
+    backgroundSlider.hide();
+    thresholdSlider.hide();
+    depthSensitivtySlider.hide();
+    pixelationSlider.hide();
+  }
 }
